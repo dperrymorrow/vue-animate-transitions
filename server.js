950ce5c60d535e21b27d1e7e4f97f53e635386ee
dperrymorrow <david.morrow@DMORROW-MBP.local>
2016-11-09 15:57:29 -0800

    demo page

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+  console.log('Server running on 8080...');
+});
